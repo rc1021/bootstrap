151a6c2b4250ffee8f6b6c2c7cd90dc6fa38c3e7
rc1021 <rc.701021@gmail.com>
2022-04-02 00:37:30 +0800

    後載入 tailwindcss 但不使 bootstrap 跑版

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./js/src/**/*.{html,js}",
+    "./site/content/**/*.{html,md,js}",
+    "./site/layouts/**/*.{html,md,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
